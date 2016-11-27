6bd8e14e405d8af33f4709b2a4926a6102c733e9
Unknown <dcarr1@gmail.com>
2016-11-27 09:21:02 -0500

    Got the mesh collider to animate with the character model.

---
+++ Assets/MeshColliderScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshColliderScript : MonoBehaviour {
+
+	MeshCollider meshCollider;
+	SkinnedMeshRenderer skinnedMeshRenderer;
+	Mesh colliderMesh;
+
+	void Start () {
+		// The mesh collider was showing up as being much bigger than the model itself.
+		// I had to put the collider on a child object so that I could change it's scale.
+		meshCollider = GetComponentInChildren<MeshCollider> ();
+		skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer> ();
+		colliderMesh = new Mesh ();
+	}
+
+	void Update () {
+		// This creates a new mesh from the current position of the character model
+		skinnedMeshRenderer.BakeMesh(colliderMesh);
+		// Assign the new mesh to the mesh collider
+		meshCollider.sharedMesh = colliderMesh;
+	}
+}

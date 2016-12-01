fdf2012f5f21fd2d51528ea6faa9f46a355b48ec
Unknown <dcarr1@gmail.com>
2016-12-01 16:48:59 -0500

    Started creating a script that makes the goal light pulsate.

---
+++ Assets/GoalLightPulsateScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalLightPulsateScript : MonoBehaviour {
+
+	public Material lightShaftMaterial;
+	public Material lightBottomMaterial;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

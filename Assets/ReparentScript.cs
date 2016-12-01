184147f5192d266a59345b99649f5c012dc86f16
Unknown <clairesquare@gmail.com>
2016-12-01 16:14:35 -0500

    Dennis added reparent script

---
+++ Assets/ReparentScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReparentScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.tag == "Ball") {
+			collider.transform.parent = transform;
+		}
+	}
+}

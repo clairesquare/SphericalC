67d182fa99c936339e22c67d3b053c87a86c7653
Unknown <dcarr1@gmail.com>
2016-12-05 11:49:47 -0500

    Added ball spawning audio.

--- Assets/Scripts/BallSpawnerScript.cs
+++ Assets/Scripts/BallSpawnerScript.cs
@@ -6,13 +6,17 @@ public class BallSpawnerScript : MonoBehaviour {
 	public GameObject ballPrefab;
 	public Color ballColor;
 
+	AudioSource audioSource;
+
 	void Start()
 	{
+		audioSource = GetComponent<AudioSource> ();
 		SpawnNewBall ();
 	}
 
 	void SpawnNewBall()
 	{
+		audioSource.Play ();
 		GameObject newBall = (GameObject) Instantiate (ballPrefab, transform.position, Quaternion.identity);
 		newBall.transform.localScale = transform.localScale;
 		newBall.GetComponent<MeshRenderer> ().material.color = ballColor;

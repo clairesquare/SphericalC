86b5283c1a609a8823e26d00629f5c4e7705bad4
Unknown <dcarr1@gmail.com>
2016-12-05 00:33:46 -0500

    Added ball rolling audio.

--- Assets/Prefabs/Ball/BallScript.cs
+++ Assets/Prefabs/Ball/BallScript.cs
@@ -8,20 +8,34 @@ public class BallScript : MonoBehaviour {
     public GameObject youWinText;
 	public GameObject youLoseText;
 
-	Transform spotlightTransform;
-
 	public bool inPlay = true;
+
+	Transform spotlightTransform;
+	AudioSource audioSource;
+	Rigidbody rigidbody;
 
 	void Start() {
 		// SPAWN A NEW SPOTLIGHT //
 		GameObject newSpotlight = (GameObject) Instantiate(spotlightPrefab, new Vector3(transform.position.x, transform.position.y + 10, transform.position.z), Quaternion.identity);
 		newSpotlight.GetComponent<BallSpotlightScript> ().ballTransform = transform;
 		spotlightTransform = newSpotlight.transform;
+
+		audioSource = GetComponent<AudioSource> ();
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+
+
+	void Update() {
+		// Set audio pitch based on current velocity
+		if (audioSource.enabled) {
+			audioSource.pitch = MyMath.Map(rigidbody.velocity.magnitude, 0f, 5f, 0.5f, 3f);
+		}
 	}
 
 
 	void RemoveFromPlay() {
 		// When this ball goes out of play delete its spotlight (And maybe do other things later)
 		Destroy(spotlightTransform.gameObject);
+		audioSource.enabled = false;
 	}
 }

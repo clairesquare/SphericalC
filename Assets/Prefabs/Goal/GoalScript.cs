42455c789d7e55dbd934de7d7792db3959d4c67e
Unknown <dcarr1@gmail.com>
2016-12-05 19:31:20 -0500

    Win screen now shows up properly in all levels.

--- Assets/Prefabs/Goal/GoalScript.cs
+++ Assets/Prefabs/Goal/GoalScript.cs
@@ -5,20 +5,17 @@ public class GoalScript : MonoBehaviour {
 
 	public GameObject winScreenGameObject;
 
-	// Delay between landing the ball in the hole and the win animation playing.
-	public float winDelay = 50f;
-
 	public AudioSource ballInAudio;
 	public AudioSource chargeAudio;
 	public AudioSource winAudio;
 
 	void OnTriggerEnter(Collider collider) {
-		if (collider.tag == "Ball" && collider.GetComponent<BallScript>().inPlay) {
+		if (collider.tag == "Ball" && collider.GetComponent<BallScript>().inPlay    ) {
 			Camera.main.BroadcastMessage ("FlashScreen");
 			ballInAudio.Play ();
 			chargeAudio.Play ();
 			GetComponentInParent<GoalLightPulsateScript> ().SendMessage ("IncreasePulseStage");
-			Invoke ("WinLevel", winDelay);
+			Invoke ("WinLevel", 5f);
 			Invoke ("StopAudio", 1.4f);
 			collider.GetComponent<BallScript> ().SendMessage ("RemoveFromPlay");
 			Destroy (collider.gameObject);
@@ -34,6 +31,7 @@ public class GoalScript : MonoBehaviour {
 	void WinLevel() {
 		winAudio.pitch = Random.Range (0.5f, 2.0f);
 		winAudio.Play ();
+		Debug.Log ("I tried to do this.");
 		winScreenGameObject.SetActive (true);
 	}
 }

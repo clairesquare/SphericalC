f80004473acfbff6e7ff1d25fa36f4d3a6ca22ff
Unknown <dcarr1@gmail.com>
2016-12-06 00:30:19 -0500

    Added cursor hiding/locking & fixed a random bug.

--- Assets/Scripts/MainMenuUIScript.cs
+++ Assets/Scripts/MainMenuUIScript.cs
@@ -25,84 +25,112 @@ public class MainMenuUIScript : MonoBehaviour {
 	}
 
 	public void LoadTrial1 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
         Time.timeScale = 1f;
 		SceneManager.LoadScene("HandLevel 1");
 	}
 
 	public void LoadTrial2 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
         Time.timeScale = 1f;
 		SceneManager.LoadScene("Pushup Level 1");
 	}
 
 	public void LoadTrial3 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
         Time.timeScale = 1f;
 		SceneManager.LoadScene("Handstand Level 3");
 	}
 
 	public void LoadTrial4 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
         Time.timeScale = 1f;
 		SceneManager.LoadScene("HandLevel 2");
 	}
 
     public void LoadTrial5 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
         Time.timeScale = 1f;
 		SceneManager.LoadScene("Pushup Level 2");
     }
 
 	public void LoadTrial6 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
 		Time.timeScale = 1f;
 		SceneManager.LoadScene("Handlevel 3");
 	}
 
 	public void LoadTrial7 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
 		Time.timeScale = 1f;
 		SceneManager.LoadScene("Yoga Level 1");
 	}
 
 	public void LoadTrial8 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
 		Time.timeScale = 1f;
 		SceneManager.LoadScene("Handstand Level 1");
 	}
 
 	public void LoadTrial9 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
 		Time.timeScale = 1f;
 		SceneManager.LoadScene("Yoga Level 2");
 	}
 
 	public void LoadTrial10 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
 		Time.timeScale = 1f;
 		SceneManager.LoadScene("Handstand Level 2");
 	}
 
 	public void LoadTrial11 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
 		Time.timeScale = 1f;
 		SceneManager.LoadScene("Pushup Level 3");
 	}
 
 	public void LoadTrial12 () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
 		Time.timeScale = 1f;
 		SceneManager.LoadScene("Yoga Level 3");
 	}
 
     public void LoadTutorial () {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		audioSource.Play ();
         Time.timeScale = 1f;
 		SceneManager.LoadScene("TutorialScene");
     }
 
 	public void LoadMainMenu () {
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
 		audioSource.Play ();
 		SceneManager.LoadScene("Title Scene");
 	}
--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -7,12 +7,16 @@ public class PauseScript : MonoBehaviour {
 	
 	void Update () {
 		if (Input.GetButtonDown("Pause")) {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
 			Time.timeScale = 0.0f;
 			pauseScreen.SetActive (true);
 		}
 	}
 
 	public void Unpause() {
+		Cursor.lockState = CursorLockMode.Confined;
+		Cursor.visible = false;
 		Time.timeScale = 1.0f;
 		pauseScreen.SetActive (false);
 	}

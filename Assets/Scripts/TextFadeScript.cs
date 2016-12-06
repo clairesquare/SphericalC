990145756f9017c4714ec8de56958f485a394de3
Unknown <dcarr1@gmail.com>
2016-12-05 21:42:29 -0500

    TextFadeScript added

---
+++ Assets/Scripts/TextFadeScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextFadeScript : MonoBehaviour {
+
+	Text text;
+	float textFadeAmount = 0.01f;
+
+	void Start () {
+		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.color = new Color (text.color.r, text.color.g, text.color.b, text.color.a - textFadeAmount*Time.deltaTime);
+		textFadeAmount *= 1.05f;
+	}
+}

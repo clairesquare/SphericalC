85d1b62b7af14215f34a6146a361f5ba5344136e
Unknown <dcarr1@gmail.com>
2016-12-05 23:34:17 -0500

    Added pause screen.

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+	public GameObject pauseScreen;
+	
+	void Update () {
+		if (Input.GetButtonDown("Pause")) {
+			Time.timeScale = 0.0f;
+			pauseScreen.SetActive (true);
+		}
+	}
+
+	public void Unpause() {
+		Time.timeScale = 1.0f;
+		pauseScreen.SetActive (false);
+	}
+}
